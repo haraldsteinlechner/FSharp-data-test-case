fa2bd486103f4d3d0c79656db4e9908a2d3a89ab
Harald Steinlechner <hs@vrvis.at>
2016-03-18 11:49:46 +0100

    init

---
+++ ConsoleApplication4/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+module Bla =
+    
+    [<ReflectedDefinition>]
+    let sum (a : int) = a + 1
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" (Bla.sum 1)
+    0 // return an integer exit code
