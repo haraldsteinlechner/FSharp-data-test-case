3d430db83643f8691285ba016af6f955a8b0ba24
Harald Steinlechner <hs@vrvis.at>
2016-04-28 16:40:46 +0200

    sync

--- ConsoleApplication4/Program.fs
+++ ConsoleApplication4/Program.fs
@@ -1,12 +1,13 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿namespace Bla
 
 module Bla =
     
     [<ReflectedDefinition>]
     let sum (a : int) = a + 1
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" (Bla.sum 1)
-    0 // return an integer exit code
+module Main =
+
+    [<EntryPoint>]
+    let main argv = 
+        printfn "%A" (Bla.sum 1)
+        0 // return an integer exit code
